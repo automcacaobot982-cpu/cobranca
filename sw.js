bb17547694d6a4c461fa894e4ea0434ddb29ed87
automcacaobot982-cpu <automcacao.bot982@gmail.com>
2026-09-11 16:22:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,19 +7,19 @@
    - CDNs (Tailwind, Chart.js, FontAwesome, Fonts): stale-while-revalidate
    Ao publicar uma nova versão do app, incremente CACHE_VERSION.
    ================================================================ */
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'v2';
 const CACHE_NAME = 'belt-cobrancas-' + CACHE_VERSION;
 
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-192-maskable.png',
-  './icons/icon-512-maskable.png',
-  './icons/apple-touch-icon.png',
-  './icons/favicon-32.png'
+  './icon-192.png',
+  './icon-512.png',
+  './icon-192-maskable.png',
+  './icon-512-maskable.png',
+  './apple-touch-icon.png',
+  './favicon-32.png'
 ];
 
 self.addEventListener('install', function (event) {
